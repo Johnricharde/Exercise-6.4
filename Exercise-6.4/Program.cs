29fc62ddcd005491de68ea9f369bd97872275733
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-22 08:25:26 +0100

    Add project files.

---
+++ Exercise-6.4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_6._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order myOrder = new Order();
+            Console.WriteLine("Input unit cost:");
+            myOrder.unitCost = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input unit count");
+            myOrder.unitCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Total cost is: {myOrder.totalPrice()}");
+        }
+            struct Order
+        {
+            public string itemName;
+            public int unitCount;
+            public int unitCost;
+            public int totalPrice() => unitCost * unitCount;
+        }
+    }
+}
